0266f5b580bad7e7ae76f5206f03bdc19d041445
Charity Wanjiku Ngocho <ngochocharity@gmail.com>
2022-06-18 23:55:48 +0300

    commit tree fe0b9644fbbefb90e2076a8911a0a2e6b763c5c8

--- index.js
+++ index.js
@@ -1,13 +1,74 @@
 /* Your Code Here */
+function createEmployeeRecord(arr){
+    let obj = new Object()
+    obj.firstName = arr[0]
+    obj.familyName = arr[1]
+    obj.title = arr[2]
+    obj.payPerHour = arr[3]
+    obj.timeInEvents = []
+    obj.timeOutEvents = []
+    return obj
+}
 
-/*
- We're giving you this function. Take a look at it, you might see some usage
- that's new and different. That's because we're avoiding a well-known, but
- sneaky bug that we'll cover in the next few lessons!
+function createEmployeeRecords (arr){
+    let newRec = []
+    arr.forEach( elem => {
+        newRec.push(createEmployeeRecord(elem))
+    })
+    return newRec
+}
 
- As a result, the lessons for this function will pass *and* it will be available
- for you to use if you need it!
- */
+function createTimeInEvent(time){
+    let newTime = time.split(" ")
+    this.timeInEvents.push(new Object({
+        type : "TimeIn",
+        date : newTime[0],
+        hour :  parseInt(newTime[1])
+    }))
+    return this
+}
+
+
+function createTimeOutEvent(time){
+    let newTime = time.split(" ")
+    this.timeOutEvents.push(new Object({
+        type : "TimeOut",
+        date : newTime[0],
+        hour :  parseInt(newTime[1])
+    }))
+    return this
+}
+
+function hoursWorkedOnDate(hrs){
+    let hours = 0
+    for(let i = 0; i < this.timeInEvents.length; i++){
+        hours += (this.timeOutEvents[i].hour - this.timeInEvents[i].hour) /100
+    }
+    // console.log(hours)
+    return hours
+}
+
+
+function wagesEarnedOnDate(givenDate) {
+  const totalHoursWorked = hoursWorkedOnDate.call(this, givenDate)
+
+  return totalHoursWorked * this.payPerHour;
+}
+
+
+const findEmployeeByFirstName =  (collection, firstNameString) => {
+  return collection.find((employee) => {
+    return employee.firstName === firstNameString;
+  })
+}
+
+
+
+let calculatePayroll = function(arrayOfEmployeeRecords){
+    return arrayOfEmployeeRecords.reduce(function(memo, rec){
+        return memo + wagesEarnedOnDate.call(rec)
+    }, 0)
+}
 
 const allWagesFor = function () {
     const eligibleDates = this.timeInEvents.map(function (e) {
@@ -20,4 +81,3 @@ const allWagesFor = function () {
 
     return payable
 }
-
